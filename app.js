e79f14ae112c515418f395bf0b981b6d6419b013
Arthur Chevalier <arthur.a.chevalier@gmail.com>
2022-10-24 17:50:12 +0200

    Save progress'

---
+++ app.js
@@ -0,0 +1,117 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Thing = require('./models/Thing');
+
+const app = express();
+//permet l'utilisation des json
+app.use(express.json());
+
+mongoose.connect(
+    'mongodb+srv://server:papi@cluster0.wpxe1ke.mongodb.net/?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');    
+    next();
+});
+
+
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    delete req.body._id;
+    const thing = new Thing({
+      ...req.body
+    });
+    thing.save()
+      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
+  
+
+
+
+
+
+
+
+
+
+module.exports = app;
+
+//////////////////////////////////////////////////////////////////
+/*
+
+
+app.use((req, res, next) => {
+    console.log('traitement');
+    next();
+})
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+})
+
+app.use((req, res, next) => 
+{
+    res.json({ message: 'requête reçue'})
+    next();
+});
+
+app.use((req, res, next) => {
+  console.log('réponse envoyée !');
+})
+
+
+*/
+
+/////////////////////////////////////////////////////
+/*
+
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'Objet créé !'
+    });
+    next();
+})
+
+*/
+
+/////////////////////////////////////////////////////
+
+/*
+app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+            _id: 'oeihfzeoi',
+            title: 'Mon premier objet',
+            description: 'Les infos de mon premier objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 4900,
+            userId: 'qsomihvqios',
+          },
+          {
+            _id: 'oeihfzeomoihi',
+            title: 'Mon deuxième objet',
+            description: 'Les infos de mon deuxième objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 2900,
+            userId: 'qsomihvqios',
+          },
+
+    ];
+    res.status(200).json(stuff);
+});
+
+*/
--- testcode.js
+++ testcode.js
@@ -1,2 +1,24 @@
 const array = [2, 4, 'nice', 9];
 console.log(array);
+
+class Cat {
+    constructor(age, cuteIndex) {
+        this._age = age;
+        this._cuteIndex = cuteIndex;
+    }
+    get age() {
+        return this._age;
+    }
+    set age(age) {
+        this._age = 1;
+    }
+    set cuteIndex(cuteIndex) {
+        this._cuteIndex = cuteIndex;
+    }
+}
+
+const cat = new Cat(4, 10);
+cat.age = 2;
+const age = cat.age;
+console.log(age)
+console.log(`Age : ${age} `)
